aa3cc4329e13952a05fe545603edd23f0f571997
gregmeszaros <meszaros275@gmail.com>
2016-01-28 18:54:12 +0000

    change folder structure, add scala basic examples

---
+++ exercises/week-4/Test.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by gergelymeszaros on 28/01/2016.
+  */
+object A {
+
+  def name(name: String) = name;
+
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println(A.name("asdasd"));
+  }
+}
