25f7a2022d3f559dd329be54f9cae4d9b95d56b2
gregmeszaros <meszaros275@gmail.com>
2016-04-10 00:23:40 +0100

    Factory design pattern+pattern matching scala week5 missing exercise

--- exercises/week-5/Week5.scala
+++ exercises/week-5/Week5.scala
@@ -23,4 +23,83 @@ object Week5 extends App {
   var customstring = "testing text in lower formatter"
   println("LowerTextFormatter: " + execute(LowerTextFormatter, customString))
 
+  val newYorkFactory = ParserFactory("NYCFactory")
+  val newYorkmsg = newYorkFactory.getParserInstance("NYCORDER")
+  newYorkmsg.parse
+
+  val LondonFactory = ParserFactory("LondonFactory")
+  val londonmsg = LondonFactory.getParserInstance("LondonFEEDBACK")
+  londonmsg.parse
+  
+
+}
+
+trait ParserFactory {
+  def getParserInstance(instance: String) : ParserInstance
+}
+
+trait ParserInstance {
+  def parse
+}
+
+object ParserFactory {
+
+  private class NYCFactory extends ParserFactory {
+    override def getParserInstance(instance: String) : ParserInstance = {
+      instance match {
+        case "NYCORDER" => new NYCORDER
+        // case "NYFEEDBACK" => new NYFEEDBACK
+        case _ => new DefaultInstance
+      }
+    }
+  }
+
+  private class LondonFactory extends ParserFactory {
+    override def getParserInstance(instance: String) : ParserInstance = {
+      instance match {
+        case "LondonFEEDBACK" => new LondonFEEDBACK
+        // case "NYFEEDBACK" => new NYFEEDBACK
+        case _ => new DefaultInstance
+      }
+    }
+  }
+
+  private class DefaultFactory extends ParserFactory {
+    override def getParserInstance(instance: String) : ParserInstance = {
+      instance match {
+        case _ => new DefaultInstance
+      }
+    }
+  }
+
+  private class NYCORDER extends ParserInstance {
+
+    println("NYC Parsing order XML...")
+
+    override def parse { println("NYC Order XML Message") }
+  }
+
+  private class LondonFEEDBACK extends ParserInstance {
+
+    println("LondonFEEDBACK Parsing feedback XML...")
+
+    override def parse { println("London feedback XML Message") }
+  }
+
+  private class DefaultInstance extends ParserInstance {
+
+    println("Default instance")
+
+    override def parse { println("Default message") }
+  }
+
+  // Our 'Factory' method
+  def apply(s: String): ParserFactory = {
+    s match {
+      case "NYCFactory" => new NYCFactory
+      case "LondonFactory" => new LondonFactory
+      case _ => new DefaultFactory
+    }
+  }
+
 }
