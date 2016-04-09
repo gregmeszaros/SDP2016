f902961903e85beef7427529f08dca4717a4a6b5
gregmeszaros <meszaros275@gmail.com>
2016-04-09 17:52:30 +0100

    Scala functional style / Strategy pattern for week5

--- exercises/week-5/Week5.scala
+++ exercises/week-5/Week5.scala
@@ -4,4 +4,23 @@ object Week5 extends App {
 
   println("week 5")
 
+  // This is scala strategy pattern using more functional style because we can pass algorithms around just like we pass objects around
+  def CapTextFormatter(inputString: String) : String = {
+    inputString.toUpperCase()
+  }
+
+  def LowerTextFormatter(inputString: String) : String = {
+    inputString.toLowerCase()
+  }
+
+  // We can use whatever function we want and they kind of behaving like objects
+  // This totally reduces the boilerplate code Strategy pattern usually produces so it makes the implementation much cleaner
+  def execute(f:(String) => String, inputString: String) = f(inputString)
+
+  var customString = "Testing text in caps formatter"
+  println("CapTextFormatter: " + execute(CapTextFormatter, customString))
+
+  var customstring = "testing text in lower formatter"
+  println("LowerTextFormatter: " + execute(LowerTextFormatter, customString))
+
 }
